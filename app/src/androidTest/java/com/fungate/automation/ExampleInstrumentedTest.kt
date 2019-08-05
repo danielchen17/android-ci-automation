e50259574a34f56019616b2de10d81413475a4f6
Daniel Chen <danielchen@emq.com>
2019-08-05 10:45:00 +0800

    update test

--- app/src/androidTest/java/com/fungate/automation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fungate/automation/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.fungate.automation
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,19 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+
     @Test
     fun useAppContext() {
+
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.fungate.automation", appContext.packageName)
+
+
+
+        onView(withText("Hello World")).check(matches(isDisplayed()))
     }
 }
